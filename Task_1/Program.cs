161b4ad2a5298f2ef9264669408872b4e4715d65
Your Name <e@w.com>
2022-08-04 23:42:13 +0300

    задача ввод двух чисел и вывод большего и меньшего

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* задача на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.*/
+Console.Write("Введите число №1: ");
+int numberfirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int numbersecond = Convert.ToInt32(Console.ReadLine());
+if (numberfirst > numbersecond)
+{
+    Console.WriteLine(numberfirst + "-большее, " + numbersecond +"-меньшее");
+}
+else if (numberfirst < numbersecond)
+{
+Console.WriteLine(numberfirst + "-меньшее, " + numbersecond + "-большее");
+}
+else
+{
+    Console.WriteLine(numberfirst + " равно " + numbersecond);
+}
