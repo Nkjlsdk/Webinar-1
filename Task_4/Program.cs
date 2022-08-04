f7d2c9904ec0ff9a2eb149c897fe29d46bd6bced
Your Name <e@w.com>
2022-08-05 02:48:49 +0300

    задача  ввод числа (N), показать все чётные числа от 1 до N

---
+++ Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// задача  ввод числа (N), на выходе показать все чётные числа от 1 до N.
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int evennumber = 0;
+while (evennumber < number)
+{
+    evennumber += 2;
+    Console.WriteLine(evennumber);
+}
