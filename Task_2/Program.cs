cbbf95433d7a294885476bf051dc460290bd51e3
Your Name <e@w.com>
2022-08-05 01:26:38 +0300

    задача вывыд максимального из трех введенных чисел

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел*/
+Console.WriteLine("Введите три числа: ");
+int numberfirst = Convert.ToInt32(Console.ReadLine());
+int numbersecond = Convert.ToInt32(Console.ReadLine());
+int numberthird = Convert.ToInt32(Console.ReadLine());
+if ((numberfirst > numbersecond) & (numberfirst > numberthird))
+{
+    Console.WriteLine("max = "+ numberfirst); 
+}
+else
+{
+    if (numbersecond > numberthird)
+    {
+         Console.WriteLine("max = "+ numbersecond);
+    }
+    else
+    {
+        Console.WriteLine("max = "+ numberthird);
+    }
+}
+    
+
