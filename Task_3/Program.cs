469148d5ead28c06725fa9fa103497688eda2a4f
Your Name <e@w.com>
2022-08-05 01:58:19 +0300

    задача на вход числа и проверить является ли оно четным

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿//  задача на вход числа и проверить является ли оно четным, делится ли оно на два без остатка
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2==0)
+{
+    Console.WriteLine(+number+" -да");
+}
+else
+{
+    Console.WriteLine(+number+" -нет");
+}
