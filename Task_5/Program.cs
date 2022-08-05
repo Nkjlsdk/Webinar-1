811dee379a87066ed93c38a768fe8c0f37d6d083
Your Name <e@w.com>
2022-08-05 17:57:43 +0300

    задача по числу опеделить день недели

---
+++ Task_5/Program.cs
@@ -0,0 +1,31 @@
+﻿/* задача по числу опеделить день недели*/
+Console.Write("Введите число : ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+if (daynumber==1)
+{
+    Console.WriteLine("Monday");
+}
+if (daynumber==2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (daynumber==3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (daynumber==4)
+{
+    Console.WriteLine("Thurstday");
+}
+if (daynumber==5)
+{
+    Console.WriteLine("Friday");
+}
+if (daynumber==6)
+{
+    Console.WriteLine("Saturday");
+}
+if (daynumber==7)
+{
+    Console.WriteLine("Sunday");
+}
